41336c9b0777b116065a82c57756e4a5549afe01
AndreaTrento95 <trentora95@gmail.com>
2021-04-09 16:07:23 +0200

    svolgimento

--- js/script.js
+++ js/script.js
@@ -1,13 +1,31 @@
 // programma che stampa numeri da 1 a 100
-var numeri_random = Math.floor((Math.random() * 100) + 1);
-console.log(numeri_random);
+/* var numeri_random = Math.floor((Math.random() * 100) + 1);
+console.log(numeri_random); */
 
 // stabilisco che per i numeri multipli di 3 e 5 venga inserito il valore alternativo
-if(numeri_random % 3 == 0){
+/* if(numeri_random % 3 === 0){
     console.log("Fizz");
-} else if(numeri_random % 5 == 0){
+} else if(numeri_random % 5 === 0){
     console.log("Buzz");
 }else{
     console.log(numeri_random);
-}
+} */
+
+/* for(i = 1; i < numeri_random.length; i++){
+    if(numeri_random % 3 === 0){
+        console.log("Fizz");
+    } else if(numeri_random % 5 === 0){
+        console.log("Buzz");
+    }else{
+        console.log(numeri_random);
+    }
+} */
+
+
+
+
+// programma che stampa i numeri da 1 a 100
+
+var arr_num = [];
+
 
