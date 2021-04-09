0989dcb2cc6be67ee86689097ce48a894b413451
AndreaTrento95 <trentora95@gmail.com>
2021-04-09 15:14:08 +0200

    svolgimento

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 var numeri_random = Math.floor((Math.random() * 100) + 1);
 console.log(numeri_random);
 
-
+// stabilisco che per i numeri multipli di 3 e 5 venga inserito il valore alternativo
 if(numeri_random % 3 == 0){
     console.log("Fizz");
 } else if(numeri_random % 5 == 0){
